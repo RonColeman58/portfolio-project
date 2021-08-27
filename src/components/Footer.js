553f6f2e94cff174b608e5e35fa96e055c492091
Ron Coleman <zendrumcorps@yahoo.com>
2021-08-27 13:01:32 -0400

    Create Ronald branch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,92 +1,93 @@
 import React from 'react'
 import {
-    FacebookShareButton,
-    FacebookIcon,
-    TwitterShareButton,
-    TwitterIcon,
-    RedditShareButton,
-    RedditIcon,
-    LinkedinShareButton,
-    LinkedinIcon
-} from "react-share";
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  RedditShareButton,
+  RedditIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+} from 'react-share'
 
 const Footer = () => {
-return (
-<div id="footer" className="footer">
-  <div className="container">
-    <div className="row">
-        <div className="col-lg-4 col-md-6 col-sm-6">
-        <div className="d-flex">
-            <p>P.O. box #### Mauldin SC, 29607</p>
-        </div>
-        <div className="d-flex">
-            <a href="tel:555-555-5555">+1(555)555-5555</a>
-        </div>
-        <div className="d-flex">
-            <p>JBridges@gmail.com</p>
-        </div>
-      </div>
-     <div className="col-lg-3 col-md-2 col-sm-6">
-      <div className="row">
-        <div className="col">
-            <a className="footer-nav">Home</a>
-            <br />
-            <a className="footer-nav">About me</a>
-            <br />
-            <a className="footer-nav">Services</a>
-        </div>
-        <div className="col">
-            <a className="footer-nav">Experience</a>
-            <br />
-            <a className="footer-nav">Portfolio</a>
-            <br />
-            <a className="footer-nav">Contacts</a>
-        </div>
-      </div>
-     </div>
-      <div className="col-lg-5 col-md-5 col-sm-6 align-item-center">
-        <div className="d-flex justify-content-center">
-            <FacebookShareButton 
-            url={"https://www.facebook.com"}
-            quote={"Where to find me"}
-            hashtag="#jbreal"
-            >
-            <FacebookIcon className="mx-3" size={36} />
-            </FacebookShareButton>
-            
-            <TwitterShareButton 
-            url={"https://www.twitter.com"}
-            quote={"Where to find me"}
-            hashtag="#jbreal"
-            >
+  return (
+    <div id="footer" className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="d-flex">
+              <p>5509 Blue Jack Oak Circle Tamarac Fl, 33319</p>
+            </div>
+            <div className="d-flex">
+              <a href="tel:555-555-5555">+1(555)555-5555</a>
+            </div>
+            <div className="d-flex">
+              <p>Zendrumcorps@gmail.com</p>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-2 col-sm-6">
+            <div className="row">
+              <div className="col">
+                <a className="footer-nav">Home</a>
+                <br />
+                <a className="footer-nav">About me</a>
+                <br />
+                <a className="footer-nav">Services</a>
+              </div>
+              <div className="col">
+                <a className="footer-nav">Experience</a>
+                <br />
+                <a className="footer-nav">Portfolio</a>
+                <br />
+                <a className="footer-nav">Contacts</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5 col-md-5 col-sm-6 align-item-center">
+            <div className="d-flex justify-content-center">
+              <FacebookShareButton
+                url={'https://www.facebook.com'}
+                quote={'Where to find me'}
+                hashtag="#jbreal"
+              >
+                <FacebookIcon className="mx-3" size={36} />
+              </FacebookShareButton>
+
+              <TwitterShareButton
+                url={'https://www.twitter.com'}
+                quote={'Where to find me'}
+                hashtag="#jbreal"
+              >
                 <TwitterIcon className="mx-3" size={36} />
-            </TwitterShareButton>
+              </TwitterShareButton>
 
-            <RedditShareButton 
-            url={"https://www.twitter.com"}
-            quote={"Where to find me"}
-            hashtag="#jbreal"
-            >
+              <RedditShareButton
+                url={'https://www.twitter.com'}
+                quote={'Where to find me'}
+                hashtag="#jbreal"
+              >
                 <RedditIcon className="mx-3" size={36} />
-            </RedditShareButton>
+              </RedditShareButton>
 
-            <LinkedinShareButton 
-            url={"https://www.linkedin.com"}
-            quote={"Where to find me"}
-            hashtag="#jbreal"
-            >
+              <LinkedinShareButton
+                url={'https://www.linkedin.com'}
+                quote={'Where to find me'}
+                hashtag="#jbreal"
+              >
                 <LinkedinIcon className="mx-3" size={36} />
-            </LinkedinShareButton>
+              </LinkedinShareButton>
+            </div>
+            <p className="pt-3 text-center">
+              Copyright&copy;
+              {new Date().getFullYear()}&nbsp; Ron Coleman Design | All Rights
+              Reserved}
+            </p>
+          </div>
         </div>
-        <p className="pt-3 text-center">
-            Copyright&copy;
-            {new Date().getFullYear()}&nbsp; JBridges Design | All Rights Reserved}
-        </p>
       </div>
     </div>
-  </div>
-</div>
-)
+  )
 }
 
 export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,31 @@
-import React from "react";
-import Typed from "react-typed";
-import {Link} from "react-scroll";
-
+import React from 'react'
+import Typed from 'react-typed'
+import { Link } from 'react-scroll'
 
 const Header = () => {
-return (
-<div id="home" className="header-wrapper">
-    <div className="main-info">
-    <h1>Pagentry Arts show development</h1>
-    <Typed 
-        className="typed-text"
-        strings={["Show Design", "Program Coordination", "Design Consultations", "Instructional Services"]}
-        delay={2000}
-        typeSpeed={60}
-        backSpeed={70}
-        loop
-    />
-    <Link smooth={true} to="contacts" href="#" className="btn-main-offer">Contacts</Link>
-    
-        </div>
+  return (
+    <div id="home" className="header-wrapper">
+      <div className="main-info">
+        <h1>Visual Design for Marching and Indoor Ensembles</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            'Show Design',
+            'Program Coordination',
+            'Design Consultations',
+            'Instructional Services',
+          ]}
+          delay={2000}
+          typeSpeed={60}
+          backSpeed={70}
+          loop
+        />
+        <Link smooth={true} to="contacts" href="#" className="btn-main-offer">
+          Contacts
+        </Link>
+      </div>
     </div>
-    )
-    }
+  )
+}
 
 export default Header
